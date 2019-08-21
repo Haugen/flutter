d85a8bcce705423b4c83400f017b5182eedbea0f
Tobias Haugen <thaugen@hugeinc.com>
2019-08-21 17:05:42 -0500

    Starting yet another app, a shop, to dive into better state management.

---
+++ shop/lib/widgets/product_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shop/screens/product_detail_screen.dart';
+
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({
+    @required this.id,
+    @required this.title,
+    this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,
+            arguments: id,
+          );
+        },
+        child: GridTile(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
